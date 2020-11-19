6431636a27e980f023a5d82040d3f0b8f811db37
sraby <samrabiyah@gmail.com>
2020-11-19 15:09:31 -0500

    Added initial line chart

--- index.ts
+++ index.ts
@@ -1,9 +1,42 @@
 import embed from "vega-embed";
-import * as Vega from "vega";
 import { EvictionTimeSeriesRow } from "./lib/eviction-time-series";
 
 async function getEvictionTimeSeries(): Promise<EvictionTimeSeriesRow[]> {
-  return (await fetch('eviction-time-series.json')).json();
+  return (await fetch("eviction-time-series.json")).json();
 }
 
-console.log("WOOO", embed, Vega, getEvictionTimeSeries());
+async function showViz() {
+  const values = await getEvictionTimeSeries();
+  const embedResult = embed("#viz", {
+    $schema: "https://vega.github.io/schema/vega-lite/v4.json",
+    description: "A simple bar chart with embedded data.",
+    width: 750,
+    height: 200,
+    data: {
+      values,
+    },
+    mark: {
+      type: "line",
+    },
+    encoding: {
+      x: {
+        field: "week",
+        type: "temporal",
+        axis: {
+          title: "",
+          format: "%b ’%y",
+          labelAngle: 45
+        },
+      },
+      y: {
+        field: "total_filings",
+        type: "quantitative",
+        axis: {
+          title: "Eviction Filings per Week",
+        },
+      },
+    },
+  });
+}
+
+showViz();
