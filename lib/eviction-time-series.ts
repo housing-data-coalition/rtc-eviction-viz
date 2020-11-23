f8fc4a83b00904557923224891660408acced62d
Atul Varma <varmaa@gmail.com>
2020-11-23 15:30:41 -0500

    Output CSV file.

--- lib/eviction-time-series.ts
+++ lib/eviction-time-series.ts
@@ -1,3 +1,9 @@
+export const EVICTION_TIME_SERIES = `eviction-time-series`;
+
+export const EVICTION_TIME_SERIES_JSON = `${EVICTION_TIME_SERIES}.json`;
+
+export const EVICTION_TIME_SERIES_CSV = `${EVICTION_TIME_SERIES}.csv`;
+
 export function convertEvictionTimeSeriesRow(row: any) {
   return {
     week: (row.week as Date).toISOString(),
@@ -12,3 +18,25 @@ export function convertEvictionTimeSeriesRow(row: any) {
 export type EvictionTimeSeriesRow = ReturnType<typeof convertEvictionTimeSeriesRow>;
 
 export type EvictionTimeSeriesNumericFields = Omit<EvictionTimeSeriesRow, "week">;
+
+export function getEvictionTimeSeriesCsvHeader(): string[] {
+  return [
+    'week',
+    'nyc_holdover_filings',
+    'nyc_nonpay_filings',
+    'outside_nyc_holdover_filings',
+    'outside_nyc_nonpay_filings',
+    'total_filings',
+  ];
+}
+
+export function toEvictionTimeSeriesCsvRow(row: EvictionTimeSeriesRow): string[] {
+  return [
+    row.week,
+    row.nyc_holdover_filings.toString(),
+    row.nyc_nonpay_filings.toString(),
+    row.outside_nyc_holdover_filings.toString(),
+    row.outside_nyc_nonpay_filings.toString(),
+    row.total_filings.toString(),
+  ];
+}
--- lib/run-queries.ts
+++ lib/run-queries.ts
@@ -1,7 +1,27 @@
-import path from "path";
+import csvStringify from "csv-stringify/lib/sync";
+
 import { existsSync, mkdirSync, readFileSync, writeFileSync } from "fs";
 import { nycdbConnector } from "./db";
-import { convertEvictionTimeSeriesRow } from "./eviction-time-series";
+import { convertEvictionTimeSeriesRow, EvictionTimeSeriesRow, EVICTION_TIME_SERIES_CSV, EVICTION_TIME_SERIES_JSON, getEvictionTimeSeriesCsvHeader, toEvictionTimeSeriesCsvRow } from "./eviction-time-series";
+
+function writeOutputFiles(data: EvictionTimeSeriesRow[]) {
+  const staticDir = 'static';
+  const jsonOutfile = `${staticDir}/${EVICTION_TIME_SERIES_JSON}`;
+  const csvOutfile = `${staticDir}/${EVICTION_TIME_SERIES_CSV}`;
+  if (!existsSync(staticDir)) {
+    mkdirSync(staticDir);
+  }
+
+  console.log(`Writing ${jsonOutfile}.`);
+  writeFileSync(jsonOutfile, JSON.stringify(data, null, 2));
+
+  console.log(`Writing ${csvOutfile}.`);
+  const lines = [getEvictionTimeSeriesCsvHeader()];
+  for (let row of data) {
+    lines.push(toEvictionTimeSeriesCsvRow(row));
+  }
+  writeFileSync(csvOutfile, csvStringify(lines));
+}
 
 export async function main() {
   const nycdb = nycdbConnector.get();
@@ -10,10 +30,5 @@ export async function main() {
   console.log(`Running SQL in ${sqlfile}.`);
   const data = (await nycdb.any(sql)).map(convertEvictionTimeSeriesRow);
   await nycdb.$pool.end();
-  const outfile = 'static/eviction-time-series.json';
-  if (!existsSync("static")) {
-    mkdirSync("static");
-  }
-  console.log(`Writing ${outfile}.`);
-  writeFileSync(outfile, JSON.stringify(data, null, 2));
+  writeOutputFiles(data);
 }
