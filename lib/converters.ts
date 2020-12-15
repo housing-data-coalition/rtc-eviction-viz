d5b7016e5eaf136ae3daabb28ad6900abd77c435
Atul Varma <varmaa@gmail.com>
2020-12-15 17:13:03 -0500

    Add comments.

--- lib/converters.ts
+++ lib/converters.ts
@@ -12,11 +12,18 @@ export function toInt(value: any): number {
   return result;
 }
 
+/**
+ * Convert the given value to either an integer or just pass it 
+ * through if it's null. Throw an error if the conversion doesn't work.
+ */
 export function toIntOrNull(value: any): number|null {
   if (value === null) return null;
   return toInt(value);
 }
 
+/**
+ * Ensure the given value is a string, throwing an error otherwise.
+ */
 export function ensureString(value: any): string {
   if (typeof(value) !== "string") {
     throw new Error(`"${value}" is not a string!`);
