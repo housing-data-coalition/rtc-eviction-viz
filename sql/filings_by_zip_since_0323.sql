3fc079e02566f1069635b52270ab7b682fbf8c65
lblok <sendtolucy@gmail.com>
2020-12-17 10:59:33 -0500

    Update filings_by_zip_since_0323.sql
    filtered for residential eviction filings only and clarified field name unitsres_2

--- sql/filings_by_zip_since_0323.sql
+++ sql/filings_by_zip_since_0323.sql
@@ -1,6 +1,3 @@
--- Updated 12/8/20 to join with residential units by zip code (except for 1-unit properties)
--- Total numbers of filings per zip are lower than previous method, unclear why.
-
 with 
 filings_zips as (
 	select 
@@ -9,7 +6,9 @@ filings_zips as (
 	from oca_index i 
 	left join oca_causes c on c.indexnumberid = i.indexnumberid
 	left join oca_addresses a on a.indexnumberid = i.indexnumberid
-	where i.fileddate >= '03-23-2020' and i.classification = any('{Holdover,Non-Payment}') and
+	where i.fileddate >= '03-23-2020' and i.classification = any('{Holdover,Non-Payment}') 
+	and propertytype = 'Residential'
+	and
 	(court = 'New York County Civil Court' or 
 		court = 'Kings County Civil Court' or 
 		court = 'Queens County Civil Court' or 
@@ -30,7 +29,7 @@ order by zipcode),
 grouped_unitsres as (
 select zipcode, 
 sum(unitsres) as unitsres_total,
-sum(unitsres) filter (where unitstotal > 1) as unitsres_2
+sum(unitsres) filter (where unitstotal > 1) as unitsrental
 from pluto_19v2
 group by zipcode
 order by zipcode)
@@ -39,17 +38,12 @@ select a.zipcode,
 filings_since_032320, 
 unitsres_total, 
 --total residential units in the zip code as per PLUTO
-unitsres_2,
+unitsrental,
 --total residential units in the zip code except for single unit properties
-filings_since_032320 * 1000 / nullif(unitsres_total, 0)::numeric as filingsrate_total, 
---filings normalized by total res units in the zip code
-filings_since_032320 * 1000 / nullif(unitsres_2, 0)::numeric as filingsrate_2plus
+filings_since_032320 * 1000 / nullif(unitsrental, 0)::numeric as filingsrate_2plus
 --filings normalized by total res units in the zip code except for single unit properties. 
 from grouped_zips a
 left join grouped_unitsres b on b.zipcode = a.zipcode 
 
 /*For map of filings by zip code, make choropleth using filingsrate_2plus
 include filings_since_032320 in tool tip*/
-
-
-
