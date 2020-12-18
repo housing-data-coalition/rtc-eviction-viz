2fd4d372ca7106b16a14756458ec9db8a374349c
lblok <sendtolucy@gmail.com>
2020-12-18 10:32:42 -0500

    field name and comment fixes
    edit data.ts to reflect new field names in filings-by-zip-since-0323.sql and update comments

--- sql/filings-by-zip-since-0323.sql
+++ sql/filings-by-zip-since-0323.sql
@@ -37,13 +37,12 @@ order by zipcode)
 select a.zipcode, 
 -- total filings since 03/23/2020
 filings_since_032320, 
--- total residential units in the zip code as per PLUTO
-unitsres_total, 
 --total residential units in the zip code as per PLUTO
-unitsrental,
+unitsres_total, 
 --total residential units in the zip code except for single unit properties
-filings_since_032320 * 1000 / nullif(unitsrental, 0)::numeric as filingsrate_2plus
+unitsrental,
 --filings normalized by total res units in the zip code except for single unit properties. 
+filings_since_032320 * 1000 / nullif(unitsrental, 0)::numeric as filingsrate_2plus
 from grouped_zips a
 left join grouped_unitsres b on b.zipcode = a.zipcode 
 
