4342be2007b2c54e8b9cb3bd2ba1979f06b83d68
lblok <sendtolucy@gmail.com>
2021-01-12 12:31:50 -0500

    fixed SQL errors

--- sql/total-active-cases.sql
+++ sql/total-active-cases.sql
@@ -1,62 +1,21 @@
-
-/*
- * with cases_active_pre_2020 as (
+ with cases_active_pre_2020 as (
 	select 
-		i.*, 
-		a.postalcode,
-		c.causeofactiontype, 
-		c.interestfromdate, 
-		c.amount, 
-		e.eventname, 
-		e.fileddate as eventfileddate, 
-		e.feetype, 
-		e.filingpartiesroles, 
-		e.answertype,
-		--date_trunc('week', i.fileddate)::date as week,
-		case when (
-		court = 'New York County Civil Court' or 
-		court = 'Kings County Civil Court' or 
-		court = 'Queens County Civil Court' or 
-		court = 'Bronx County Civil Court' or 
-		court = 'Richmond County Civil Court' or 
-		court = 'Redhook Community Justice Center' or 
-		court = 'Harlem Community Justice Center')
-		 then 'NYC' 
-		else 'Outside NYC' end as region,
-		case when ( court = 'New York County Civil Court') 
-		then 'Manhattan' else 'Not Manhattan' end as manhattan
+		i.*
 	from oca_index i 
-	left join oca_causes c on c.indexnumberid = i.indexnumberid
-	left join oca_events e on e.indexnumberid = i.indexnumberid 
-	left join oca_addresses a on a.indexnumberid = i.indexnumberid
-	where i.fileddate < '01-01-2020' 
-	and status = 'Active'
+	where i.fileddate < '01-01-2020' -- case was filed before 1/1/20
+	and (disposeddate >= '01-01-2020' or status like '%Active%') -- case is active or was disposed after 1/1/20, i.e. active on 1/1/20
 	and i.classification = any('{Holdover,Non-Payment}') 
-	order by i.fileddate asc 
+	order by i.fileddate desc
 	)
-	
-	select count(*) from cases_active_pre_2020
-	
-	
-	Result: 183,173
- */
 
+select count(*) as pre20count from cases_active_pre_2020;
 
-
+-- Result: 167,889
 
 
 with covid_res_filings as (	
 	select 
-		i.*, 
-		a.postalcode,
-		c.causeofactiontype, 
-		c.interestfromdate, 
-		c.amount, 
-		e.eventname, 
-		e.fileddate as eventfileddate, 
-		e.feetype, 
-		e.filingpartiesroles, 
-		e.answertype,
+		i.*,
 		date_trunc('week', i.fileddate)::date as week_filed,
 		date_trunc('week', disposeddate)::date as week_disposed,
 		case when (
@@ -68,13 +27,8 @@ with covid_res_filings as (
 		court = 'Redhook Community Justice Center' or 
 		court = 'Harlem Community Justice Center')
 		 then 'NYC' 
-		else 'Outside NYC' end as region,
-		j.*
-	from oca_index i 
-	left join oca_causes c on c.indexnumberid = i.indexnumberid
-	left join oca_events e on e.indexnumberid = i.indexnumberid 
-	left join oca_addresses a on a.indexnumberid = i.indexnumberid
-	left join oca_judgments j on j.indexnumberid = i.indexnumberid 
+		else 'Outside NYC' end as region
+	from oca_index i  
 	where i.fileddate >= '01-01-2020' and i.classification = any('{Holdover,Non-Payment}') 
 	order by i.fileddate asc),
 
@@ -97,7 +51,7 @@ order by week_disposed),
 cumulative as (
 select week_filed as day, 
 cases_filed, 
-183173 + sum(cases_filed) over (order by week_filed) as cum_filed,
+167889 + sum(cases_filed) over (order by week_filed) as cum_filed,
 cases_disposed,
 sum(cases_disposed) over (order by week_filed) as cum_disposed
 from grouped_week_filed 
@@ -109,4 +63,3 @@ cum_filed - cum_disposed as active_cases
 from cumulative
 where day > '01-01-2020' and
 day is not null
-	
