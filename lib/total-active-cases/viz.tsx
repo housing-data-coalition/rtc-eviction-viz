dfe2b6e5b329f795f78c2080230a1c7774e78e1b
lblok <sendtolucy@gmail.com>
2021-02-09 16:40:39 -0500

    another syntax fix change num type to any

--- lib/total-active-cases/viz.tsx
+++ lib/total-active-cases/viz.tsx
@@ -45,7 +45,7 @@ const ActiveCasesViz: React.FC<ActiveCasesVizProps> = (props) => {
   );
 };
 
-function thousands_separators(num)
+function thousands_separators(num: any)
   {
     var num_parts = num.toString().split(".");
     num_parts[0] = num_parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
