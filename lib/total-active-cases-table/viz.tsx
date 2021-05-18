727aabe38542cb173c30da992d1532b81adbfb49
Samara Trilling <samara.trilling@gmail.com>
2021-05-18 17:07:13 -0400

    Refactor

--- lib/total-active-cases-table/viz.tsx
+++ lib/total-active-cases-table/viz.tsx
@@ -13,65 +13,8 @@ export const ActiveCasesTable: React.FC<{}> = () => {
     );
 };
 
-function constructDataTable(easyAccessMap:any) {
-    var table = Array.from(Array(9), () => Array(3)); // 9 rows, 3 cols
-    table[4][1] = easyAccessMap['nyc-prepandemic-residential']['Non-Payment']
-    table[4][2] = easyAccessMap['nyc-prepandemic-residential']['Holdover']
-    table[5][1] = easyAccessMap['nyc-pandemic-residential']['Non-Payment']
-    table[5][2] = easyAccessMap['nyc-pandemic-residential']['Holdover']
-    table[7][1] = easyAccessMap['outside-prepandemic-all']['Non-Payment']
-    table[7][2] = easyAccessMap['outside-prepandemic-all']['Holdover']
-    table[8][1] = easyAccessMap['outside-pandemic-all']['Non-Payment']
-    table[8][2] = easyAccessMap['outside-pandemic-all']['Holdover']
-
-    // Sum outside NYC vertically
-    table[6][1] = table[7][1] + table[8][1]
-    table[6][2] = table[7][2] + table[8][2]
-    // Sum outside NYC horizontally
-    table[6][0] = table[6][1] + table[6][2]
-    table[7][0] = table[7][1] + table[7][2]
-    table[8][0] = table[8][1] + table[8][2]
-
-    // Sum NYC vertically
-    table[3][1] = table[4][1] + table[5][1]
-    table[3][2] = table[4][2] + table[5][2]
-    // Sum NYC horizontally
-    table[3][0] = table[3][1] + table[3][2]
-    table[4][0] = table[4][1] + table[4][2]
-    table[5][0] = table[5][1] + table[5][2]
-
-    // Sum Statewide vertically
-    table[1][1] = table[4][1] + table[7][1]
-    table[1][2] = table[4][2] + table[7][2]
-    table[2][1] = table[5][1] + table[8][1]
-    table[2][2] = table[5][2] + table[8][2]
-    // Sum Statewide horizontally
-    table[1][0] = table[4][0] + table[7][0]
-    table[2][0] = table[5][0] + table[8][0]
-    
-    // Top line vertical
-    table[0][1] = table[1][1] + table[2][1]
-    table[0][2] = table[1][2] + table[2][2]
-    // Top line horizontal
-    table[0][0] = table[0][1] + table[0][2]
-    return table;
-}
-
 const ActiveCasesTableWithValues: React.FC<{values: TotalActiveCasesRow[]}> = (values) => {
-    var activeCasesMap = Object.fromEntries(new Map(values.values.map(key => [key.category, key.count] as [string, number])));
-    var activeCasesDeepObject: any = unflatten(activeCasesMap);
-    console.log(activeCasesDeepObject);
-
-    const easyAccessMap = {
-        'outside-pandemic-all': activeCasesDeepObject['Outside NYC']['Issued Pandemic']['All'],
-        'outside-prepandemic-all': activeCasesDeepObject['Outside NYC']['Issued Prepandemic']['All'],
-        'nyc-pandemic-residential': activeCasesDeepObject['NYC']['Issued Pandemic']['Residential'], 
-        'nyc-pandemic-commercial': activeCasesDeepObject['NYC']['Issued Pandemic']['Commercial'],
-        'nyc-prepandemic-residential': activeCasesDeepObject['NYC']['Issued Prepandemic']['Residential'],
-        'nyc-prepandemic-commercial': activeCasesDeepObject['NYC']['Issued Prepandemic']['Commercial'] 
-    }
-
-    const dataTable = constructDataTable(easyAccessMap);
+    const dataTable = constructDataTable(values);
 
     type ActiveCasesDisplayRow = {id: string, totalActiveCases: number, nonPayment: number, holdover: number}
     const rowLabels = [
@@ -86,7 +29,7 @@ const ActiveCasesTableWithValues: React.FC<{values: TotalActiveCasesRow[]}> = (v
         'Pandemic #'
     ]
     var data = [];
-    for (var i=0;i<rowLabels.length;i++) {
+    for (var i = 0; i < rowLabels.length; i++) {
         data[i] = {
             id: rowLabels[i],
             totalActiveCases: dataTable[i][0],
@@ -150,3 +93,65 @@ const ActiveCasesTableWithValues: React.FC<{values: TotalActiveCasesRow[]}> = (v
         </table>
     )
 };
+
+
+function constructDataTable(values: React.PropsWithChildren<{
+    values: TotalActiveCasesRow[];
+}>) {
+    var activeCasesMap = Object.fromEntries(new Map(values.values.map(key => [key.category, key.count] as [string, number])));
+    var activeCasesDeepObject: any = unflatten(activeCasesMap);
+
+    const easyAccessMap = {
+        'outside-pandemic-all': activeCasesDeepObject['Outside NYC']['Issued Pandemic']['All'],
+        'outside-prepandemic-all': activeCasesDeepObject['Outside NYC']['Issued Prepandemic']['All'],
+        'nyc-pandemic-residential': activeCasesDeepObject['NYC']['Issued Pandemic']['Residential'], 
+        'nyc-pandemic-commercial': activeCasesDeepObject['NYC']['Issued Pandemic']['Commercial'],
+        'nyc-prepandemic-residential': activeCasesDeepObject['NYC']['Issued Prepandemic']['Residential'],
+        'nyc-prepandemic-commercial': activeCasesDeepObject['NYC']['Issued Prepandemic']['Commercial'] 
+    }
+    var table = Array.from(Array(9), () => Array(3)); // 9 rows, 3 cols
+    return fillTable(table, easyAccessMap);
+}
+
+function fillTable(table: number[][], easyAccessMap: {[key: string]: {'Non-Payment': number, 'Holdover': number}}) {
+    table[4][1] = easyAccessMap['nyc-prepandemic-residential']['Non-Payment']
+    table[4][2] = easyAccessMap['nyc-prepandemic-residential']['Holdover']
+    table[5][1] = easyAccessMap['nyc-pandemic-residential']['Non-Payment']
+    table[5][2] = easyAccessMap['nyc-pandemic-residential']['Holdover']
+    table[7][1] = easyAccessMap['outside-prepandemic-all']['Non-Payment']
+    table[7][2] = easyAccessMap['outside-prepandemic-all']['Holdover']
+    table[8][1] = easyAccessMap['outside-pandemic-all']['Non-Payment']
+    table[8][2] = easyAccessMap['outside-pandemic-all']['Holdover']
+
+    // Sum outside NYC vertically
+    table[6][1] = table[7][1] + table[8][1]
+    table[6][2] = table[7][2] + table[8][2]
+    // Sum outside NYC horizontally
+    table[6][0] = table[6][1] + table[6][2]
+    table[7][0] = table[7][1] + table[7][2]
+    table[8][0] = table[8][1] + table[8][2]
+
+    // Sum NYC vertically
+    table[3][1] = table[4][1] + table[5][1]
+    table[3][2] = table[4][2] + table[5][2]
+    // Sum NYC horizontally
+    table[3][0] = table[3][1] + table[3][2]
+    table[4][0] = table[4][1] + table[4][2]
+    table[5][0] = table[5][1] + table[5][2]
+
+    // Sum Statewide vertically
+    table[1][1] = table[4][1] + table[7][1]
+    table[1][2] = table[4][2] + table[7][2]
+    table[2][1] = table[5][1] + table[8][1]
+    table[2][2] = table[5][2] + table[8][2]
+    // Sum Statewide horizontally
+    table[1][0] = table[4][0] + table[7][0]
+    table[2][0] = table[5][0] + table[8][0]
+    
+    // Top line vertical
+    table[0][1] = table[1][1] + table[2][1]
+    table[0][2] = table[1][2] + table[2][2]
+    // Top line horizontal
+    table[0][0] = table[0][1] + table[0][2]
+    return table;
+}
