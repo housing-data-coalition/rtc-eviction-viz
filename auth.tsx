a2fcf119f0945fc815158d68860f6b9589cca144
zhik <zhikhe@gmail.com>
2022-01-14 21:25:32 -0500

    add auth via cookies

---
+++ auth.tsx
@@ -0,0 +1,23 @@
+
+import React from "react";
+import useCookie from "react-use-cookie";
+
+interface AuthContextType {
+  isAuthenticated: string;
+  signin: (password: string, callback: VoidFunction) => void;
+}
+
+export const AuthContext = React.createContext<AuthContextType>(null!);
+
+export function AuthProvider({ children }: { children: React.ReactNode }) {
+  let [isAuthenticated, setAuthentication] = useCookie("auth", "null");
+
+  let signin = (password: string, callback: VoidFunction) => {
+    setAuthentication(password);
+    callback();
+  };
+
+  let value = { isAuthenticated, signin };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+}
---
+++ contants.ts
@@ -0,0 +1,12 @@
+export const VIEW_WIDGET = "widget";
+export const VIEW_CONFIGURE_WIDGET = "config";
+
+export const QS_VIEW = "view";
+export const QS_FIELD_NAME = "fieldName";
+export const QS_HEIGHT = "height";
+
+export const EVICTION_VIZ_DEFAULT_HEIGHT = 150;
+export const ACTIVE_CASES_VIZ_DEFAULT_HEIGHT = 500;
+export const JUDGMENTS_VIZ_DEFAULT_HEIGHT = 500;
+
+export const REACT_APP_PASSWORD = process.env.REACT_APP_PASSWORD || "password"
