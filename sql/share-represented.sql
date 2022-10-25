94029feddbcb10940d102c0a33214597e0143005
lblok <sendtolucy@gmail.com>
2022-10-25 15:34:28 -0400

    Update share-represented.sql

--- sql/share-represented.sql
+++ sql/share-represented.sql
@@ -1,11 +1,18 @@
--- select non-payment and holdover cases' respondents and representation types, filed during pandemic, that have had an appearance
--- note this query has more info than needed for a chart of share of represented tenants over time, it includes partial representation 
-with cases_plus_rep as (
+-- select non-payment and holdover cases' respondents and representation types, filed after the eviction moratorium expired, that have had their first appearance within the last week
+with first_appearance as (
+    select 
+    	indexnumberid, 
+    	min(appearancedatetime) as appearancedatetime
+    from oca_appearances
+    group by indexnumberid
+),
+cases_plus_rep as (
 	select 
 		oi.*,
-		representationtype from oca_index oi  
+		representationtype 
+	from oca_index oi  
 	left join oca_parties op using(indexnumberid)
-	left join oca_case_summary ocs using(indexnumberid)
+	inner join first_appearance using(indexnumberid)
 	where oi.classification in ('Non-Payment','Holdover')
 		and role = 'Respondent' 
 		and propertytype = 'Residential'
@@ -19,19 +26,18 @@ with cases_plus_rep as (
 						Redhook Community Justice Center,
 						Harlem Community Justice Center
 					}')
+		-- grab everything after the eviction moratorium ended
 		and fileddate > '2022-01-15'
-		and appearances_num > 0
+		and appearancedatetime < current_date - interval '1 weeks'
 ),
 -- select just relevant fields and eliminate duplicates
 all_cases as (
 	select 
 		indexnumberid, 
-		classification, 
-		fileddate,
 		date_trunc('week', fileddate)::date as day, 
 		string_agg(distinct representationtype, ', ') as representationtype
 	from cases_plus_rep
-	group by indexnumberid, classification, fileddate, day
+	group by indexnumberid, day
 )
 -- group by week filed and calculated representation rate 
 select 
@@ -45,7 +51,7 @@ select
 	-- Calculate percent of cases that had representation
 	round((count(*) filter (where representationtype != 'SRL'))::numeric*100 / count(*), 1) as rep_rate
 from all_cases
--- grab everything until 4 weeks before current date to account for missing recent data 
+-- grab everything after the eviction moratorium ended
 where day < current_date - interval '4 weeks'
 group by day
-order by day; 
+order by day;
