2aaa72c96253e91bf043b3db82d339d9f8f35c9e
lblok <sendtolucy@gmail.com>
2022-12-21 11:44:42 -0500

    update methodology to deduplicate appearances

--- sql/share-represented.sql
+++ sql/share-represented.sql
@@ -1,11 +1,19 @@
--- select non-payment and holdover cases' respondents and representation types, filed during pandemic, that have had an appearance
--- note this query has more info than needed for a chart of share of represented tenants over time, it includes partial representation 
-with cases_plus_rep as (
+
+-- select appearances where at least two have occurred at least one week ago, because tenants should have attorneys after two appearances. methodology updated 12/21/22
+with appears_twice as (
+	select indexnumberid, count(distinct(indexnumberid,appearancedatetime)) from oca_appearances
+	where appearancedatetime < current_date - interval '1 weeks'
+	group by indexnumberid 
+	having count(distinct(indexnumberid,appearancedatetime)) > 1 -- updated 12/22 to account for duplicate first appearancesd
+),
+-- select non-payment and holdover cases' respondents and representation types, filed after the eviction moratorium expired, that have had their first appearance within the last week
+cases_plus_rep as (
 	select 
 		oi.*,
-		representationtype from oca_index oi  
+		representationtype 
+	from oca_index oi  
 	left join oca_parties op using(indexnumberid)
-	left join oca_case_summary ocs using(indexnumberid)
+	inner join appears_twice using(indexnumberid)
 	where oi.classification in ('Non-Payment','Holdover')
 		and role = 'Respondent' 
 		and propertytype = 'Residential'
@@ -19,23 +27,21 @@ with cases_plus_rep as (
 						Redhook Community Justice Center,
 						Harlem Community Justice Center
 					}')
+		-- grab everything after the eviction moratorium ended
 		and fileddate > '2022-01-15'
-		and appearances_num > 0
-),
+		),
 -- select just relevant fields and eliminate duplicates
 all_cases as (
 	select 
 		indexnumberid, 
-		classification, 
-		fileddate,
 		date_trunc('week', fileddate)::date as day, 
 		string_agg(distinct representationtype, ', ') as representationtype
 	from cases_plus_rep
-	group by indexnumberid, classification, fileddate, day
+	group by indexnumberid, day
 )
 -- group by week filed and calculated representation rate 
 select 
-	day, 
+   	day,
 	-- Count cases with "Self-Represented Litigants" only
 	count(*) filter (where representationtype = 'SRL') as srl,
 	-- Count cases with representation (or partial representation)
@@ -45,7 +51,8 @@ select
 	-- Calculate percent of cases that had representation
 	(count(*) filter (where representationtype != 'SRL'))::numeric*100 / count(*) as rep_rate
 from all_cases
--- grab everything until 4 weeks before current date to account for missing recent data 
+-- grab everything after the eviction moratorium ended
 where day < current_date - interval '4 weeks'
-group by day
-order by day; 
+group by day 
+having count(*) > 100 -- only include weeks with over 100 total cases 
+order by day
