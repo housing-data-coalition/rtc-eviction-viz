7bbb903d386a1c243ec64ad5858ec160a09d098b
Maxwell Austensen <m.austensen@gmail.com>
2023-03-21 12:53:55 -0400

    use temporary table and addd index before slow aggregation

--- sql/share-represented.sql
+++ sql/share-represented.sql
@@ -1,43 +1,48 @@
--- select appearances where at least two have occurred at least one week ago, because tenants should have attorneys after two appearances. methodology updated 12/21/22
-with appears_twice as (
-	select indexnumberid, count(distinct(indexnumberid,appearancedatetime)) from oca_appearances
-	where appearancedatetime < current_date - interval '1 weeks'
-	group by indexnumberid 
-	having count(distinct(indexnumberid,appearancedatetime)) > 1 -- updated 12/22 to account for duplicate first appearancesd
-),
--- select non-payment and holdover cases' respondents and representation types, filed after the eviction moratorium expired, that have had their first appearance within the last week
-cases_plus_rep as (
-	select 
-		oi.*,
-		representationtype 
-	from oca_index oi  
-	left join oca_parties op using(indexnumberid)
-	inner join appears_twice using(indexnumberid)
-	where oi.classification in ('Non-Payment','Holdover')
-		and role = 'Respondent' 
-		and propertytype = 'Residential'
-		and representationtype != 'No Appearance' -- filter out no appearances 
-		and oi.court = any('{
-						Bronx County Civil Court,
-						Kings County Civil Court,
-						New York County Civil Court,
-						Queens County Civil Court,
-						Richmond County Civil Court,
-						Redhook Community Justice Center,
-						Harlem Community Justice Center
-					}')
-		-- grab everything after the eviction moratorium ended
-		and fileddate > '2022-01-15'
-		),
--- select just relevant fields and eliminate duplicates
-all_cases as (
+
+create temp table x_all_cases as (
+
+	-- select appearances where at least two have occurred at least one week ago, because tenants should have attorneys after two appearances. methodology updated 12/21/22
+	with appears_twice as (
+		select indexnumberid, count(distinct(indexnumberid,appearancedatetime)) from oca_appearances
+		where appearancedatetime < current_date - interval '1 weeks'
+		group by indexnumberid 
+		having count(distinct(indexnumberid,appearancedatetime)) > 1 -- updated 12/22 to account for duplicate first appearancesd
+	), 
+	-- select non-payment and holdover cases' respondents and representation types, filed after the eviction moratorium expired, that have had their first appearance within the last week
+	cases_plus_rep as (
+		select 
+			oi.*,
+			representationtype 
+		from oca_index oi  
+		left join oca_parties op using(indexnumberid)
+		inner join appears_twice using(indexnumberid)
+		where oi.classification in ('Non-Payment','Holdover')
+			and role = 'Respondent' 
+			and propertytype = 'Residential'
+			and representationtype != 'No Appearance' -- filter out no appearances 
+			and oi.court = any('{
+							Bronx County Civil Court,
+							Kings County Civil Court,
+							New York County Civil Court,
+							Queens County Civil Court,
+							Richmond County Civil Court,
+							Redhook Community Justice Center,
+							Harlem Community Justice Center
+						}')
+			-- grab everything after the eviction moratorium ended
+			and fileddate > '2022-01-15'
+	)
+	-- select just relevant fields and eliminate duplicates
 	select 
 		indexnumberid, 
 		date_trunc('week', fileddate)::date as day, 
 		string_agg(distinct representationtype, ', ') as representationtype
 	from cases_plus_rep
 	group by indexnumberid, day
-)
+);
+
+create index on x_all_cases (indexnumberid, day);
+
 -- group by week filed and calculated representation rate 
 select 
    	day,
@@ -49,9 +54,9 @@ select
 	count(*) as allcases,
 	-- Calculate percent of cases that had representation
 	round((count(*) filter (where representationtype != 'SRL'))::numeric*100 / count(*), 1) as rep_rate
-from all_cases
+from x_all_cases
 -- grab everything after the eviction moratorium ended
 where day < current_date - interval '4 weeks'
 group by day 
 having count(*) > 100 -- only include weeks with over 100 total cases 
-order by day
+order by day;
