5986f47c8e782e8ea3e663bbcbf7a278ec721fc2
Zhi Keng He <zhikhe@gmail.com>
2023-11-01 21:04:58 -0400

    add base url for pr-preview-action

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/rtc-eviction-viz',
+  base: process.env.BASE_URL ?? '/rtc-eviction-viz',
   define: {
     'process.env': process.env
   }
